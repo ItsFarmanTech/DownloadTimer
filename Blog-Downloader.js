0b2d891cf35527db04dfc84aaf27eb96165f9dde
FarruQureshi <148589878+ItsFarmanTech@users.noreply.github.com>
2023-12-07 18:35:43 +0530

    Update Blog-Downloader.js

--- Blog-Downloader.js
+++ Blog-Downloader.js
@@ -3,8 +3,8 @@ function checkUniqueId() {
   var gdriveLink = "";
   if (uniqueId === "1ZPGS82IU9XZ6WT7") {
     gdriveLink = "https://drive.google.com/drive/folders/1Yg1ATn27BaL4U1QZ3irAQMmCPqOYbeMr";
-  } else if (uniqueId === "ID2") {
-    gdriveLink = "Link2";
+  } else if (uniqueId === "VPVNH0FYFG7FUADZ") {
+    gdriveLink = "https://drive.google.com/drive/u/1/folders/1IJECHliPuDZ1AoQSSqtHeJyiNVwxOP7c";
   }
 
   if (gdriveLink !== "") {
